d8efac2c904d835c2530ce81d1e4b898797adeb2
Elbert Chan <elb.chan@gmail.com>
2022-03-16 17:32:26 -0700

    Didn't get very far. Got mySQL connected to Node.

--- server/db/index.js
+++ server/db/index.js
@@ -9,4 +9,24 @@ var mysql = require('mysql2');
 // OR
 // user: 'root', password: 'some_password_you_created_at_install'
 
+// create the connection to the database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test'
+});
 
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
+connection.query(
+  'select * from testTable;',
+  (err, results, fields) => {
+    console.log(results);
+    //console.log(fields);
+    // console.log('Is this running?');
+    // Elbert, you need to remember to distinguish between the DATABASE and the TABLES
+  }
+);
